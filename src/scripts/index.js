b96d1f0c53c7e121b1aba88d01e979e1a704b109
Muhammad Qurtubi <qurtubi1411@gmail.com>
2024-08-10 09:49:49 +0700

    local storage page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,17 +8,21 @@ function saveToLocalStorage(key, value) {
     localStorage.setItem(key, value);
 }
 
-
 function loadFromLocalStorage(key, defaultValue) {
     return localStorage.getItem(key) || defaultValue;
 }
 
 
+function saveCurrentPage(page) {
+    saveToLocalStorage("currentPage", page);
+}
+
+
 window.addEventListener("scroll", () => {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    navbar.style.opacity=0.7
+    navbar.style.opacity = 0.7;
     if (scrollTop > lastScroll) {
-        navbar.style.top = "-100px"; 
+        navbar.style.top = "-100px";
     } else {
         navbar.style.top = "0";
     }
@@ -27,11 +31,11 @@ window.addEventListener("scroll", () => {
         clearTimeout(scrollTimeout);
     }
 
-    
     scrollTimeout = setTimeout(() => {
         navbar.style.opacity = 1;
-    }, 200); 
+    }, 200);
 });
+
 
 async function fetchData(page, pageJumlah, publish) {
     try {
@@ -53,29 +57,33 @@ async function fetchData(page, pageJumlah, publish) {
     }
 }
 
+
 async function card() {
     const sortPage = document.getElementById("sortPage");
     const sortPublish = document.getElementById("sortDate");
-    const show=document.getElementById("show");
+    const show = document.getElementById("show");
+
     
     sortPage.value = loadFromLocalStorage("sortPage", "10");
     sortPublish.value = loadFromLocalStorage("sortPublish", "new");
-    
+    currentPage = loadFromLocalStorage("currentPage", 1);
     async function updateCard() {
         const page = currentPage;
         const pageJumlah = sortPage.value;
         const publish = sortPublish.value;
-        show.innerText=pageJumlah
+        show.innerText = pageJumlah;
         let resultPublish = publish === "new" ? "-published_at" : "published_at";
-        
+
         
         saveToLocalStorage("sortPage", pageJumlah);
         saveToLocalStorage("sortPublish", publish);
+        saveCurrentPage(page); 
 
         const data = await fetchData(page, pageJumlah, resultPublish);
         const listCard = document.getElementById("list-card");
-        
+
         listCard.innerHTML = data.data.map((item) => {
+            console.log(item.medium_image[0].url);
             return `
             <div class="aspect-[9/11] bg-white rounded-lg shadow-lg overflow-hidden">
                 <img src="${item.medium_image[0].url}" alt="${item.title}" class="w-full aspect-video">
@@ -90,16 +98,19 @@ async function card() {
 
     sortPage.addEventListener("change", async () => {
         currentPage = 1; 
+        saveCurrentPage(currentPage); 
         await updateCard();
     });
 
     sortPublish.addEventListener("change", async () => {
         currentPage = 1; 
+        saveCurrentPage(currentPage); 
         await updateCard();
     });
 
     updateCard();
 }
+
 
 function renderPagination(links) {
     containerPagination.innerHTML = links.map((item) => {
@@ -118,10 +129,11 @@ function renderPagination(links) {
             const url = button.getAttribute('data-url');
             if (url) {
                 currentPage = new URLSearchParams(url).get('page[number]');
+                saveCurrentPage(currentPage); 
                 card(); 
             }
         });
     });
 }
 
-card();
+card(); 
